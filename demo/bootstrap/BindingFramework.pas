f99a5f6dc45fbb82f5f7229822cdadbf67b5ba30
Michael Justin <info@habarisoft.com>
2018-04-18 11:45:43 +0200

    Use Bootstrap 4

--- demo/bootstrap/BindingFramework.pas
+++ demo/bootstrap/BindingFramework.pas
@@ -76,25 +76,26 @@ begin
       [rfReplaceAll]);
 
   Result := StringReplace(Result,
-      '#{bootstrap.home}',
-      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6',
+      '<bs:bootstrap_css />',
+      '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">',
       [rfReplaceAll]);
 
   Result := StringReplace(Result,
-      '#{jquery.min.js}',
-      'https://code.jquery.com/jquery-1.10.2.min.js',
+      '<bs:bootstrap_js />',
+      '<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>' + #13
+    + '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>' + #13
+    + '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>' + #13,
       [rfReplaceAll]);
 
   Result := StringReplace(Result,
       '<bs:footer />',
       '<footer class="footer" id="contact">'
-      + '      <div class="row">'
-      + '          <div class="col-md-12">'
-      + '                  <a href="http://getbootstrap.com/">Bootstrap</a> (C) 2015 Twitter, licensed under MIT License. Open Sans licensed under Apache License, version 2.0'
-      + '              </p>'
-      + '          </div>'
-      + '      </div>'
-      + '  </footer>',
+    + '    <div class="row">'
+    + '        <div class="col-md-12">'
+    + '            <a href="http://getbootstrap.com/">Bootstrap</a> Copyright (c) 2011-2018 Twitter, Inc., Copyright (c) 2011-2018 The Bootstrap Authors, licensed under MIT License. Open Sans licensed under Apache License, version 2.0'
+    + '        </div>'
+    + '    </div>'
+    + '</footer>',
       []);
 
 end;
