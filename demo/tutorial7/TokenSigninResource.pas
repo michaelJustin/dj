0926e321648fffc9c02219d678390b2ff2bc7e74
michaelJustin <michael.justin@habarisoft.com>
2019-02-27 12:52:13 +0100

    no message

--- demo/tutorial7/TokenSigninResource.pas
+++ demo/tutorial7/TokenSigninResource.pas
@@ -1,7 +1,7 @@
 ﻿(*
 
     Daraja Framework
-    Copyright (C) 2016  Michael Justin
+    Copyright (C) Michael Justin
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
