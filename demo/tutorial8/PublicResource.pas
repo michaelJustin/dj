8ae1f82cd51eed187f311d9d0804e709815a105f
michaelJustin <michael.justin@habarisoft.com>
2019-03-04 12:08:17 +0100

    no message

--- demo/tutorial8/PublicResource.pas
+++ demo/tutorial8/PublicResource.pas
@@ -58,6 +58,9 @@ begin
     Response.Redirect(MY_CALLBACK_URL)
   end else begin
     Credentials := ToCredentials(Request.Session.Content.Values['credentials']);
+
+    WriteLn('expires_in: ' + IntToStr(Credentials.expires_in));
+
     if Credentials.expires_in <= 0 then begin
       Response.Redirect(MY_CALLBACK_URL)
     end else begin
