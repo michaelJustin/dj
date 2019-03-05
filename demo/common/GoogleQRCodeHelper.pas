14271cbf138ba678ea94cbacd8e5f4761b391fd4
michaelJustin <michael.justin@habarisoft.com>
2019-03-05 15:16:36 +0100

    no message

--- demo/common/GoogleQRCodeHelper.pas
+++ demo/common/GoogleQRCodeHelper.pas
@@ -1,5 +1,5 @@
 (*
-   Copyright 2016 Michael Justin
+   Copyright (C) Michael Justin
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
