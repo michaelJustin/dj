e9a7043181fe15888670ea11746fb0407137cf57
Michael Justin <info@habarisoft.com>
2019-05-26 12:27:15 +0200

    2.0(.rc1)

--- source/djGlobal.pas
+++ source/djGlobal.pas
@@ -33,7 +33,7 @@ interface
 {$i IdCompilerDefines.inc}
 
 const
-  DWF_SERVER_VERSION = '1.2.10';
+  DWF_SERVER_VERSION = '2.0.rc1';
   DWF_SERVER_FULL_NAME = 'Daraja Framework ' + DWF_SERVER_VERSION;
   DWF_SERVER_COPYRIGHT = 'Copyright (C) Michael Justin';
 
